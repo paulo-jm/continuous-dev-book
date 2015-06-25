8fa906f00d1f662de7fd93fef37ea225a03c206b
pauojm <pauojm@pauojm-K46CB>
2015-06-25 11:01:38 -0300

    add code

---
+++ geekseek-api/src/main/java/org/arquillian/example/Greeter.java
@@ -0,0 +1,25 @@
+package org.arquillian.example;
+
+import java.io.PrintStream;
+
+import javax.inject.Inject;
+
+public class Greeter {
+
+	
+	PhraseBuilder phraseBuilder;
+	
+	@Inject
+	public Greeter(PhraseBuilder phraseBuilder) {
+		this.phraseBuilder = phraseBuilder;
+	}
+	
+	public void greet(PrintStream to, String name) {
+        to.println(createGreeting(name));
+    }
+
+    public String createGreeting(String name) {
+        return phraseBuilder.buildPhrase("hello", name);
+    }
+
+}
